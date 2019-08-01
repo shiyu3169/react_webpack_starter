10b5ab96b3b1e6669feb620561be8ac1f513e7e7
Shiyu Wang <shiyu3169@gmail.com>
2019-08-01 15:42:36 -0400

    fix version

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
-ReactDOM.render(<APP />, document.querySelector("#app"));
+ReactDOM.render(<App />, document.querySelector("#app"));
